087878779d0f16f5f8d7431a65b45c1ba9e4395c
Angus Urquhart <aurquhart@pmsi-consulting.com>
2017-03-24 18:45:28 +0000

    Progress on iterating on urls

--- url_looping.py
+++ url_looping.py
@@ -9,6 +9,7 @@ Created on Wed Mar 22 07:05:30 2017
 from collections import defaultdict
 
 import trackopy
+import pandas as pd
 user = {"password":'e479deb2e1', 'username':'still-gnoll-6604'}
 trackobot = trackopy.Trackobot(user['username'], user['password'])
 
@@ -17,16 +18,31 @@ trackobot = trackopy.Trackobot(user['username'], user['password'])
 
 #Create list of urls
 yourdict = {}
-yourlist = []
+list_of_game_dicts = []
 
 for x in range(1,8):
-    
+
      yourdict[1] = trackobot.history(page=x)
-     yourlist.append(yourdict.copy())
- 
-print (yourlist[6])
+     list_of_game_dicts.append(yourdict.copy())
+
+page_counter = 0
+counter = 0
+
+page = list_of_game_dicts[0]
+game = game1[1]['history'][counter]['card_history']
+game = pd.DataFrame(game)
+# break out the dict that is one of the fields in the dataframe
+game = pd.concat([game, game['card'].apply(pd.Series)], axis=1)
+# delete the original dict field
+del game['card']
 
 
-#for x in range (1,11):
-#    print('page='+str(x))
-    
+
+#These can be used to explore structure of json
+#game1.keys()
+#game1[1].keys()
+#game1[1]['meta']
+#game1[1]['history']
+
+
+print(game)
